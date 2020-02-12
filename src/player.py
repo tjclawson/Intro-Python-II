bebada68c6228da8b7e5b781bafef7e2ea9d16b0
tjclawson <tjamesclawson@gmail.com>
2020-02-12 17:37:56 -0500

    Add drop_item method to player class and invoke item.on_take() when user adds item to inventory

--- src/player.py
+++ src/player.py
@@ -11,3 +11,16 @@ class Player:
     def pickup_item(self, item):
         if item is not None:
             self.inventory.append(item)
+            item.on_take()
+
+    def drop_item(self, item_name):
+        try:
+            for i in self.inventory:
+                if i.name == item_name:
+                    item = i
+            self.inventory.remove(item)
+            item.on_drop()
+            return item
+        except UnboundLocalError:
+            print(f"You do not have {item_name} in your inventory")
+
