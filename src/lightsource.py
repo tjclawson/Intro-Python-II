0ea447d5729dd701f4368eff6b5babd71718da9a
tjclawson <tjamesclawson@gmail.com>
2020-02-12 18:53:21 -0500

    Add LightSource class

---
+++ src/lightsource.py
@@ -0,0 +1,8 @@
+from item import Item
+
+class LightSource(Item):
+    def __init__(self, name, description):
+        super().__init__(name, description)
+
+    def on_drop(self):
+        print("It is not wise to drop your source of light!")
