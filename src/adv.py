b38c6ffb10c332c76c9035f2d4eca0fe6131f68a
tjclawson <tjamesclawson@gmail.com>
2020-02-12 16:34:23 -0500

    Implement new item methods in adv.py

--- src/adv.py
+++ src/adv.py
@@ -88,11 +88,7 @@ while True:
 
     else:
         if input_list[0] == "get" or "take":
-            try:
-                item = [item for item in current_room.items if item.name == input_list[1]][0]
-                player.inventory.append(item)
-                item.on_take()
-                current_room.items.remove(item)
-            except IndexError:
-                print(f"{input_list[1]} is not an item in this room!")
+            player.pickup_item(current_room.get_item(input_list[1]))
+
+
 
