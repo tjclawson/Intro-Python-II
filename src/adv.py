753b1c988193213cc176b3800048dadce01c26ef
tjclawson <tjamesclawson@gmail.com>
2020-02-13 18:29:29 -0500

    Replace while loop with battle object and method call

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,6 @@
 from player import Player
 from world import World
-
+from battle import Battle
 
 #
 # Main
@@ -15,32 +15,16 @@ player = Player(f"{user_name}", world.room["outside"])
 
 
 while True:
-    current_room = player.current_room
-    current_room.is_room_illuminated = False
+    player.current_room.is_room_illuminated = False
 
-    if current_room.is_light or current_room.contains_lightsource() or player.has_lightsource():
-        current_room.is_room_illuminated = True
+    if player.current_room.is_light or player.current_room.contains_lightsource() or player.has_lightsource():
+        player.current_room.is_room_illuminated = True
 
-    print(current_room)
+    print(player.current_room)
 
-    while current_room.monster is not None and current_room.is_room_illuminated:
-        print("\nTHERE IS A MONSTER IN THIS ROOM!")
-        print(current_room.monster)
-        option = input('What will you do? Use "attack" to attack the monster, use "run" to run away: ')
-        if option == "run":
-            current_room = player.last_room
-            player.current_room = player.last_room
-        elif option == "attack":
-            if player.has_weapon():
-                current_room.monster.on_attack()
-                if current_room.monster.hp == 0:
-                    current_room.monster = None
-                    print("You defeated the monster!")
-                    print(current_room)
-            else:
-                print("You do not have a weapon to attack with!")
-        else:
-            print("Please enter a valid command")
+    if player.current_room.monster is not None:
+        player = Battle(player, player.current_room.monster).battle()
+        print(player.current_room)
 
     user_input = input(">>> ")
     input_list = user_input.split()
@@ -62,11 +46,11 @@ while True:
         action = input_list[0]
         item_name = input_list[1]
         if action == "get" or action == "take":
-            picked_item = current_room.get_item(item_name)
+            picked_item = player.current_room.get_item(item_name)
             player.pickup_item(picked_item)
         elif action == "drop":
             dropped_item = player.drop_item(item_name)
-            current_room.add_item(dropped_item)
+            player.current_room.add_item(dropped_item)
 
 
 
