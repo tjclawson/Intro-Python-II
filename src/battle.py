6fb0ea032760853185d6ff0c887c8bcebfba6e02
tjclawson <tjamesclawson@gmail.com>
2020-02-13 19:06:56 -0500

    Add logic to have monster attack player and break the loop player HP hits 0

--- src/battle.py
+++ src/battle.py
@@ -17,6 +17,7 @@ class Battle:
     def battle(self):
         while self.monster.hp != 0:
             self.print_monster_presence()
+            print(f"{self.player.name}: {self.player.hp}HP")
             option = input('What will you do? Use "attack" to attack the monster, use "run" to run away: ')
             if option == "run":
                 self.player.current_room = self.player.last_room
@@ -27,6 +28,12 @@ class Battle:
                     if self.monster.hp == 0:
                         self.player.current_room.monster = None
                         print("You defeated the monster!")
+                    print(f"{self.monster.name} is attacking!")
+                    self.player.on_attack()
+                    if self.player.hp == 0:
+                        for i in range(1, 10):
+                            print("GAME OVER")
+                        break
                 elif self.player.has_weapon() and not self.player.current_room.is_room_illuminated:
                     print("You cannot attack the monster because you cannot see!")
                 else:
